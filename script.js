c174774665cce7d02d4d6c44c74869bded762e9a
Shivdasbhande05 <shivdasbhande@gmail.com>
2025-01-09 08:45:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If required.
 const student = {
 	name : "John Doe",
-	getKeys : function () {
-		return Object.keys(this);
-	}
 };
+Object.prototype.getKeys = function () {
+	return Object.keys(this);
+}
 
 console.log(student.getKeys());
